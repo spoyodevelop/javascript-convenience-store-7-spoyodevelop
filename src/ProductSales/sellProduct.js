8b16bf5562b518cf6efd3e6045cf0f68b6481c0e
spoyodevelop <kjy9505@gmail.com>
2024-11-11 21:45:37 +0900

    fix(sellProduct): 오타 수정

--- src/ProductSales/sellProduct.js
+++ src/ProductSales/sellProduct.js
@@ -54,7 +54,7 @@ export async function adjustQuantities(
       remainder + nonPromoSellQuantity
   ) {
     wantToBuyNonPromo = await inputView.askUserAgree(
-      `현재 ${name}은(는) ${remainder + nonPromoSellQuantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
+      `현재 ${name} ${remainder + nonPromoSellQuantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
     );
   }
 
