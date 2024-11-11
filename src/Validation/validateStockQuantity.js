b8373427f9be9be4468bf2b43b02b20a50434728
spoyodevelop <kjy9505@gmail.com>
2024-11-11 13:57:51 +0900

    refactor(validateStockQuantity): validateStockQuantity를 여러 함수로 쪼갬

--- src/Validation/validateStockQuantity.js
+++ src/Validation/validateStockQuantity.js
@@ -1,33 +1,42 @@
 import { DateTimes } from '@woowacourse/mission-utils';
 import findProduct from '../ProductFinder/findProduct.js';
 
+function isValidProduct(foundProduct) {
+  if (!Array.isArray(foundProduct) || foundProduct.length === 0) return false;
+  return true;
+}
+
+function calculatePromoQuantity(foundProduct) {
+  const promoProduct = foundProduct.find((product) => product.isPromoProduct());
+
+  if (!promoProduct || !promoProduct.isAvailablePromotion(DateTimes.now()))
+    return 0;
+
+  return promoProduct.quantity;
+}
+
+function calculateNonPromoQuantity(foundProduct) {
+  const nonPromoProduct = foundProduct.find(
+    (product) => !product.isPromoProduct(),
+  );
+
+  if (!nonPromoProduct) return 0;
+
+  return nonPromoProduct.quantity;
+}
+
 export default function validateStockQuantity(shoppingCart, parsedProducts) {
-  for (const item of shoppingCart) {
+  return shoppingCart.every((item) => {
     const foundProduct = findProduct(parsedProducts, item.name);
 
-    if (!Array.isArray(foundProduct) || foundProduct.length === 0) {
-      return false;
-    }
+    if (!isValidProduct(foundProduct)) return false;
 
-    const promoProduct = foundProduct.find((product) =>
-      product.isPromoProduct(),
-    );
-    const nonPromoProduct = foundProduct.find(
-      (product) => !product.isPromoProduct(),
-    );
-
-    let promoQuantity = promoProduct ? promoProduct.quantity : 0;
-    const nonPromoQuantity = nonPromoProduct ? nonPromoProduct.quantity : 0;
-
-    if (promoProduct && !promoProduct.isAvailablePromotion(DateTimes.now())) {
-      promoQuantity = 0;
-    }
-
+    const promoQuantity = calculatePromoQuantity(foundProduct);
+    const nonPromoQuantity = calculateNonPromoQuantity(foundProduct);
     const totalAvailableQuantity = promoQuantity + nonPromoQuantity;
 
-    if (Number(item.quantity) > totalAvailableQuantity) {
-      return false;
-    }
-  }
-  return true;
+    if (Number(item.quantity) > totalAvailableQuantity) return false;
+
+    return true;
+  });
 }
